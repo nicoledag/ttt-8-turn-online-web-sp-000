eb8c2563ae765ba6d19bd16779d6eae5ef8a8b0f
Nicole D <nnardella@gmail.com>
2018-10-01 13:41:58 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,7 +6,41 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+
 def input_to_index(user_input)
    converted_input = user_input.to_i - 1
    converted_input
+end
+
+
+def valid_move?(board, index)
+
+if index.between?(0, 8) && !position_taken?(board, index)
+
+  return true
+else
+ return false
+ end
+end
+
+def position_taken?(board, index)
+
+if board[index] == " "
+  return false
+
+elsif board[index] == ""
+  return false
+
+elsif board[index] == nil
+  return false
+
+else board[index] == "X" || "O"
+  return true
+
+ end
+end
+
+
+def move(board, index, char="X")
+  board[index] = char
 end
